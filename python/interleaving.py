8711899599196298553b0f0b387af2abe6f018d8
Aaron L <kitsunay34@gmail.com>
2018-11-09 13:47:07 -0700

    Update interleaving.py
    corrected typo, added more documentation

--- python/interleaving.py
+++ python/interleaving.py
@@ -21,23 +21,30 @@ def inter2(l1, l2):
 
 # ========== Variadic amount of parameters ==========
 def vinter_tools(*lists):
+    '''iterators are powerful and concise, and more often than not are the most performant solution'''
     return [x for x in itertools.chain(
         *itertools.zip_longest(
             *lists)) if x is not None]
-
+    
 
 def vinter_preallocated(*lists):
-    temp = [None] * (max(map(len, lists)) * len(lists))
+    '''preallocates a list of the desired size and populates using one input list at a time, then a final step cleans up the default values
+    _Default prevents collisions with lists containing None or other useful primitive datatypes, but does carry some overhead
+    this is a fairly cache-friendly way to iterate, so while not as fast as builtins, still is performant'''
+    _Default = type('_Default', (), {})
+    temp = [_Default] * (max(map(len, lists)) * len(lists))
     N = len(lists)
 
     for i, l in enumerate(lists):
         for j, item in enumerate(l):
             temp[i + j*N] = item
-    temp = [x for x in temp if x is not None]
+    temp = [x for x in temp if x is not _Default]
     return temp
 
 
 def vinter_procedural(*lists):
+    '''this is the natural generalization of inter1 to variadics
+    it has the notable issue of traversing each list even after i >= len'''
     temp = []
     for i in range(max(map(len, lists))):
         for l in lists:
@@ -47,9 +54,7 @@ def vinter_procedural(*lists):
 
 
 def vinter_procedural2(*lists):
-    # unnecessary conditional branches ar the kryptonite of branch predictors
-    # by culling from the input set as you go, conditional branches are removed
-    # hence the sudden drop in execution time
+    '''by culling from the iteration set as you go, execution time improves significantly'''
 
     temp = []
     valid = list(lists)
